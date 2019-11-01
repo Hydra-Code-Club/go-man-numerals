1e56ec16bd555fce07294bcc4d29e057e83b72ec
Chris Hallberg <crhallberg@gmail.com>
2019-11-01 10:31:46 -0400

    Day 2!
    - toString working
    - Combinations added by hand
    - Remaining mysteries: globals, type templates, pointers

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
   "fmt"
   s "strings"
+  "sort"
 )
 
 type numeral struct {
   value int
 }
 
-func getDigitMap() map[byte]int {
+func getCharToValueTable() map[byte]int {
   // TODO: instance
   digits := make(map[byte]int)
   digits['I'] = 1
@@ -21,22 +22,58 @@ func getDigitMap() map[byte]int {
   digits['M'] = 1000
   return digits
 }
+func getValueToCharTable() map[int]string {
+  m := getCharToValueTable()
+  n := make(map[int]string)
+  for k, v := range m {
+      n[v] = string(k)
+  }
+  n[4] = "IV"
+  n[9] = "IX"
+  n[40] = "XL"
+  n[400] = "CD"
+  n[900] = "CM"
+  return n
+}
+
+// TODO: map keys helper function (templates!)
+
+// TODOn't: validating helper function
 
 func (n numeral) getNumber() int {
   return n.value
 }
-/*
-func (n *numeral) toString() string {
-  return n.value
+
+func (n numeral) toString() string {
+  charTable := getValueToCharTable()
+  // get keys
+  keys := make([]int, len(charTable))
+  i := 0
+  for k := range charTable {
+    keys[i] = k
+    i++
+  }
+  // sort
+  sort.Ints(keys)
+  // iterate
+  num := n.getNumber()
+  str := ""
+  for i := len(keys) - 1; i >= 0; i-- { // RTL
+    for num >= keys[i] {
+      num -= keys[i]
+      str += string(charTable[keys[i]])
+    }
+  }
+  return str
 }
-*/
+
 func parseString(str string) numeral {
-  digits := getDigitMap()
+  digitTable := getCharToValueTable()
   upper := s.ToUpper(str)
   sum := 0
   highest := 0
   for i := len(upper) - 1; i >= 0; i-- { // RTL
-    val := digits[upper[i]]
+    val := digitTable[upper[i]]
     if val >= highest {
       sum += val
       highest = val
@@ -45,6 +82,7 @@ func parseString(str string) numeral {
     }
   }
 
+  // TIL: You don't need to specify key. Set by order.
   return numeral{value: sum}
 }
 
@@ -54,4 +92,6 @@ func main() {
   num := parseString("XIV")
 
   fmt.Println(num.getNumber())
+
+  fmt.Println(num.toString())
 }
--- main_test.go
+++ main_test.go
@@ -9,10 +9,25 @@ func parseTest(t *testing.T, str string, expected int) {
   }
 }
 
-func TestTesting(t *testing.T) {
+// TIL: Capitalized vars and functions are exported from the package
+func TestParsing(t *testing.T) {
   parseTest(t, "X", 10)
   parseTest(t, "XI", 11)
   parseTest(t, "IX", 9)
   parseTest(t, "MCMLXXVII", 1977)
   parseTest(t, "IIIIIIIIX", 2)
+}
+
+func encodeTest(t *testing.T, num int, expected string) {
+  tmp := numeral{num}.toString()
+  if (tmp != expected) {
+    t.Errorf("%d not %s, was %s", num, expected, tmp)
+  }
+}
+func TestEncoding(t *testing.T) {
+  encodeTest(t, 10, "X")
+  encodeTest(t, 11, "XI")
+  encodeTest(t, 9, "IX")
+  encodeTest(t, 1977, "MCMLXXVII")
+  encodeTest(t, 2, "II")
 }
